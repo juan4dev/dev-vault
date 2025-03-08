26105d782b2b61e8b36d6e499b466803e4679d58
juan4dev <72524993+juan4dev@users.noreply.github.com>
2025-03-08 20:20:39 +0100

    Add abstract Repository class for entity management

---
+++ dev-vault/courses/project/Catalogo/src/Catalogo.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,23 @@
+﻿using Catalogo.Domain.Abstractions;
+
+namespace Catalogo.Infrastructure.Repositories;
+
+internal abstract class Repository<T> where T : Entity
+{
+    protected readonly CatalogoDbContext _context;
+
+    public Repository(CatalogoDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(T entity, CancellationToken cancellationToken = default)
+    {
+        await _context.Set<T>().AddAsync(entity, cancellationToken);
+    }
+
+    public virtual async Task<T?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        return await _context.Set<T>().FindAsync([id], cancellationToken);
+    }
+}
